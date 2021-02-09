26a789ab145e0df5d22f69ca823fc1a12f8d3aea
Dainius <dainius1112@gmail.com>
2021-02-10 00:35:35 +0200

    start db connect semi layout route try

---
+++ resources/views/login/login/main.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.layout')
+
+@section('content')
+    <form action="{{ route('login') }}" method="post">
+        <div>
+            <label for="email">El. paštas</label>
+            <input type="mail" name='email'>
+        </div>
+        <div>
+            <label for="password">Slaptažodis</label>
+            <input type="password" name='password'>
+        </div>
+        {{-- {!! Form::submit($text, [$options]) !!} --}}
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/login',[LoginController::class, 'index'])->name('login');
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('gallery.main');
 });
